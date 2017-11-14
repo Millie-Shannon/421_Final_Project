703b61282ad1cd3baa0cbd7c2de6519e0185f08f
Millie-Shannon <syf1@rice.edu>
2017-11-14 16:25:45 -0600

    Light and pressure sensor adjusted for circuit playgroun

--- Detection/Detection.ino
+++ Detection/Detection.ino
@@ -1,38 +1,40 @@
 int nopress = 0; 
 // int dn2 = 0; 
-const int lightpin = 8;
-int relaypin = 10;  
+const int lightpin = 2;
+// int relaypin = 10;  
+int lightpin_input = 10; 
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 pinMode(lightpin, OUTPUT);
 digitalWrite(lightpin, LOW); 
-pinMode(relaypin, OUTPUT);
-digitalWrite(relaypin, LOW);
+//pinMode(relaypin, OUTPUT);
+//digitalWrite(relaypin, LOW);
+pinMode(lightpin_input, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(A0);
-  //Serial.println(sensorValue); 
+  int sensorValue = analogRead(10);
+  Serial.println(sensorValue); 
   delay(200); 
 
-  if (sensorValue == 0) {
+  if (sensorValue <= 5) {
     nopress = nopress + 1; 
     //Serial.println(nopress);
   } 
 
    
   if (nopress >= 10) {
-  Serial.println("I'm awake!"); 
+  //Serial.println("I'm awake!"); 
   delay(200); 
   digitalWrite(lightpin,HIGH);
-  digitalWrite(relaypin,HIGH); 
+ // digitalWrite(relaypin,HIGH); 
 //  dn2 = 0; 
   }
   if (nopress < 10) {
-  Serial.println("I'm still asleep.");
+ // Serial.println("I'm still asleep.");
   delay(200);  
   digitalWrite(lightpin,LOW);
  }   
